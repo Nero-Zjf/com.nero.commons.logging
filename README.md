ad560c8f85d984c3aab1e4e8a015673a3f31ec68
ZengJunFeng <zengjunfeng9981@163.com>
2020-03-24 09:51:12 +0800

    初始提交

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# com.nero.commons.logging
+# commons logging demo
+文档：Commons-logging-基本介绍和使用
+基本使用
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Main {
+    public static final Log log = LogFactory.getLog(Main.class);
+    public static void main(String[] args) {
+        log.info("---------info-----------");
+        log.debug("---------debug-----------");
+    }
+}
